6f802e8a65bdf3d7a66f202aeaf73df57ecab21b
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-09 00:39:36 +0500

    updating Arithmetic  file

--- 01_basics/04_arithmetic_operations.py
+++ 01_basics/04_arithmetic_operations.py
@@ -1,2 +1,13 @@
 # Arithmetic operations like +, -, *, /, //, %, and ** are supported in Python.
 
+a =10 
+b =2
+
+print("Addition:", a + b)          # Addition
+print("Subtraction:", a - b)       # Subtraction
+print("Multiplication:", a * b)    # Multiplication
+print("Division:", a / b)          # Division
+
+print("Floor Division:", a // b)   # Floor Division
+print("Modulus:", a % b)           # Modulus   
+print("Exponentiation:", a ** b)   # Exponentiation
