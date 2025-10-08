01c6645fa65efb94041a6718d37a06c39a3d7c54
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-09 00:56:27 +0500

    Adding the data types

---
+++ 01_basics/01_data_types.py
@@ -0,0 +1,38 @@
+'''
+python support 3 categories of data types
+1. Basic data types (int, float, complex, bool, str)
+2. Collection data types (list, tuple, set, dict)
+3. User-defined data types (class, object)
+'''
+# integers
+print("Integer data type:" ,42)
+
+
+# float
+print("Float data type:", 3.14)
+
+# bolean
+print("Boolean data type:", True)
+print("Boolean data type:", False)
+
+
+# complex
+print("Complex data type:", 2 + 3j)
+
+# String 
+print("String data type:", "Hello, Python!")
+print('String data type:', 'Hello, Python!')
+
+
+# Container data types / Collection data types
+# list 
+print("List data type:", [1, 2, 3, 4, 5])
+
+# tuple
+print("Tuple data type:", (1, 2, 3, 4, 5))
+
+# set
+print("Set data type:", {1, 2, 3, 4, 5})
+
+# dictionary
+print("Dictionary data type:", {"name": "Ahsan", "age": 30  }) # key-value pair
