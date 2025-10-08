6cc535207b88dc568d3f22496528db3c8ac2f523
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-09 00:47:06 +0500

    updating file

---
+++ 01_basics/00_hello_world.py
@@ -0,0 +1,20 @@
+print("Hello, World! from Ahsan Raza")
+print(200 + 300)
+
+print("Pakistan","India", "Bangladesh", sep=" | ")  # sep is used to separate the values with a specific character or string
+
+print("Pakistan","India", "Bangladesh", sep=" /")  # sep is used to separate the values with a specific character or string
+
+print("Pakistan","India", "Bangladesh", sep=" ,")  # sep is used to separate the values with a specific character or string
+
+# This is a comment
+# print("This line is commented out and won't run") 
+
+"""
+This is a multi-line comment
+It can span multiple lines
+"""
+print("This line will run")
+
+
+
