91c6ecc65b60208f23063eaea84e94cefa19b0d5
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-10 00:57:40 +0500

    adding the problem 3

---
+++ Python-Problems/03_problem.py
@@ -0,0 +1,13 @@
+'''
+Task
+The provided code stub reads an integer, , from STDIN. For all non-negative integers , print .
+
+Example
+
+The list of non-negative integers that are less than n=4 is [0,1,2] . Print the square of each number on a separate line.
+'''
+
+n = int(input())
+
+for i in range(n):
+    print(i*i)   
