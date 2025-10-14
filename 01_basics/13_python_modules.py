4a66f2ca71596f5309f1ced587f363dfedc24598
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-14 23:30:35 +0500

    Uploading the modules file

---
+++ 01_basics/13_python_modules.py
@@ -0,0 +1,16 @@
+'''
+what are the modules?
+-> A file contains a set of functions you want to incude in your appications
+1. Math
+2. Random
+3. OS
+4. time
+'''
+import math # this is used to perform mathametical operations in side your program
+
+print(math.pi) #this will print the value of pi
+print(math.factorial)
+print(math.e)
+print(math.ceil(0.5))
+print(math.floor(0.6))   
+print(math.sqrt(100))   
