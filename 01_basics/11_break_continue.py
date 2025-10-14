f1865c960416f65e070da5ab1d63b8468c692602
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-14 21:47:28 +0500

    Uploading the break continue file

---
+++ 01_basics/11_break_continue.py
@@ -0,0 +1,16 @@
+# Break
+# Break is used to terminate the loop at specific point where the condition full filled
+for i in range(1,11):
+    if i == 5: 
+        break
+    print(i)
+
+# Continue
+# In continue statment when the give condition is fullfilled then the particual ittration will execute the code after the given condition in the following code when the i == 5 on that spot it will not execute the print statement and the moves to the next itration
+
+for i in range(1,11):
+    if i == 5: 
+        continue
+    print(i)
+
+
