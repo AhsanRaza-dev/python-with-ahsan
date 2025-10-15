354810404f1b8a81b99df944b36ad6c15363c749
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-15 22:28:58 +0500

    Uploading the strings sclicing  file

---
+++ 01_basics/15_string_slicing.py
@@ -0,0 +1,8 @@
+# String Slicing
+a = "Ahsan Raza your python instructor"
+print(a[0:6]) # to print Ahsan
+print(a[6:11]) # to print Raza
+print(a[1:]) # to print hsan Raza your python instructor skipping the A
+print(a[1:6:2]) # That will print the char from the string 1 to 6 index but there is twist the 2 mention means that skip one between them it will only show "ha"
+
+print(a[::-1]) # that will reves the string
