5e4fc81aa094321ad19ef13c189ff95f0ff0151b
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-15 22:15:47 +0500

    Uploading the strings accessing types  file

--- 01_basics/14_strings.py
+++ 01_basics/14_strings.py
@@ -11,3 +11,20 @@ print(c)
 
 # ------------------------------------------------------------------------
 # Accessing substrings from the string 
+
+# 1. concept indexing
+d = "Pakistan"
+# from d we want to fetch the k from the pakistan then we will use the following syntex
+
+print(d[2])   # this will bring the K from the string the index starts from 0,1,2,3,4..........n 
+
+
+# Types of Indexing
+# 1. Positive Indexing
+
+print(d[2]) 
+
+# 2. Negative Indexing
+e = 'Python'
+print(e[-2]) # this will print the o from the given string
+
