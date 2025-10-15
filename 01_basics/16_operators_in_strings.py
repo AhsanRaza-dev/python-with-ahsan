aa4c1de9e4eb1ab083a691ea81252d46cddf34d3
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-15 23:32:00 +0500

    Uploading the strings operation files

---
+++ 01_basics/16_operators_in_strings.py
@@ -0,0 +1,28 @@
+'''
+operations in python
+1. Arthmatic
+2. Relational 
+3. Logical
+4. Loops on strings
+5. Membership
+'''
+
+a = 'hello' + 'hello'  # you can add multipule + here
+b = 'hello' +'-'+ 'WORLD'
+print(a) #that will concate the both strings
+print(b)
+
+print("*"*50)  # that will print 50 '*'
+
+
+print("hello"=="World")  # that will print the False 
+print("hello"!="World")  # that will print the True 
+print("Lahore">"World")  # that will print the False 
+
+
+c = "Hi this is Ahsan Raza"
+for i in c: 
+    print(i)
+
+for i in c[::-1]: 
+    print(i) 
