daa3d37f9f14589357e72aa737ad8cd19af612d5
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-15 22:06:59 +0500

    Uploading the strings file

---
+++ 01_basics/14_strings.py
@@ -0,0 +1,13 @@
+a = "Hello, world"
+b = 'HEllo, World'
+c = '''HEllo this is Ahsan Raza your personal tutor for this course comment below to ask anything!'''
+
+print(a)
+print(b)
+print(c)
+
+
+# these are the ways to use the strings
+
+# ------------------------------------------------------------------------
+# Accessing substrings from the string 
