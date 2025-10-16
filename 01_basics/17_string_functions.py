5c7ec9d88b80c1704ead6e927884cdc678f61710
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-16 22:14:53 +0500

    Uploading the string specific functions files

--- 01_basics/17_string_functions.py
+++ 01_basics/17_string_functions.py
@@ -1,4 +1,4 @@
-a = 'Lahore'
+a = 'lahore'
 
 # common funtions
 print(len(a))
@@ -6,3 +6,23 @@ print(max(a))
 print(min(a))
 
 print(sorted(a, reverse=True))
+
+# String Specific functions
+
+# 1. Captilization
+print(a.capitalize()) # this will captilize the first char
+
+# 2. title
+print(a.title()) # this will captilize the entire string
+
+# 3. upper
+print(a.upper())  #it will convert the entire string in to capital
+
+# 4. lower
+print(a.lower())  #it will convert the entire string in to Lower
+
+
+# 5. count
+b = 'This is the python course which will help you getting the skills in the field of AI'.count('t')
+print(b)
+
