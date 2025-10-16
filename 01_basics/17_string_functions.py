496cb8993449d1bd46c38f057ce5f91b9f17329b
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-16 22:27:16 +0500

    Uploading the string specific functions files

--- 01_basics/17_string_functions.py
+++ 01_basics/17_string_functions.py
@@ -23,6 +23,16 @@ print(a.lower())  #it will convert the entire string in to Lower
 
 
 # 5. count
-b = 'This is the python course which will help you getting the skills in the field of AI'.count('t')
+b = 'This is the python course which will help you in getting the skills in the field of AI'.count('t')
+print(b)
+
+# 6. Find/index
+b = 'This is the python course which will help you in getting the skills in the field of AI'.find('t') #it will give you the index of first occurance of the char
+print(b) 
+
+
+# 7. format
+# it is used where we don't know which value will come
+b = 'This is the {} course which will help you in getting the skills in the field of {}'.format('python','AI')
 print(b)
 
